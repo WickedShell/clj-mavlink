62c350aee44e13a0d9dff55c82b0028dfa3a0080
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-03-19 19:36:54 -0700

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-mavlink "0.1.0-SNAPSHOT"
+  :description "Clojure MAVLink bindings"
+  :url "https://github.com/WickedShell/clj-mavlink"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
