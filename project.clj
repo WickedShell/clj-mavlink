db2b929ee43cc2941e7dad10797b598fec8f7f8a
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-03-20 02:38:28 -0700

    Add data.xml dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/WickedShell/clj-mavlink"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.xml "0.0.8"]])
