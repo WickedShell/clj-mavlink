1b38483226dcd93fe59cfb2f711a0dc7304c9ef7
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-02-11 22:15:28 -0700

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-mavlink "0.1.1-SNAPSHOT"
+(defproject clj-mavlink "0.1.1"
   :description "Clojure MAVLink"
   :url "https://github.com/WickedShell/clj-mavlink"
   :license {:name "Eclipse Public License"
